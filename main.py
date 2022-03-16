6a7098a49bde2342d50a173946c4eb19f7ca5e8a
kozlovskaya-oa <94537045+kozlovskaya-oa@users.noreply.github.com>
2022-03-16 16:58:32 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
